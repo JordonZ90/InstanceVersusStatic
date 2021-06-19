fcd5af009fc5c6b93c96a0b4d5d6b61091f6edf3
Vik <jzenterprize90@gmail.com>
2021-06-19 00:00:42 -0400

    Demo comparison of static v. instance fields

---
+++ InstanceVersusStatic/Panda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstanceVersusStatic
+{
+    public class Panda
+    {
+        public string Name; // Instance field
+        public static int Population; // Static field
+        // Population pertains to all instances of Panda
+
+        public Panda(string name) // Consturctor
+        {
+            Name = name; // Assign the instance field
+            Population += 1; // Increment the static field
+        }
+    }
+}
---
+++ InstanceVersusStatic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InstanceVersusStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pandas();
+        }
+        public static void Pandas()
+        {
+            Panda p1 = new Panda("Pan Dee");
+            Panda p2 = new Panda("Pan Dah");
+
+            Console.WriteLine(p1.Name);
+            Console.WriteLine(p2.Name);
+
+            Console.WriteLine($"Total population of panda's: '{Panda.Population}'");
+        }
+    }
+}
